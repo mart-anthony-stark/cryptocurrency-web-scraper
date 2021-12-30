b279a7335725cca537475c8165eede29b4c129c1
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-30 23:19:45 +0800

    Add news scraper

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -1,12 +1,11 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-//sc-1eb5slv-0 iworPT - name
-// sc-131di3y-0 cLgOOr - price
-const URL = "https://coinmarketcap.com/";
-const selector =
-  "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
+
 module.exports = {
-  async getAll(req, res) {
+  async getTopCoins(req, res) {
+    const URL = "https://coinmarketcap.com/";
+    const selector =
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
     const response = await axios.get(URL);
     const html = response.data;
     const $ = cheerio.load(html);
@@ -39,5 +38,34 @@ module.exports = {
       }
     });
     res.json({ cryptoCurrencies });
+  },
+
+  async getNews(req, res) {
+    const URL =
+      "https://www.investing.com/news/cryptocurrency-news?utm_source=google&utm_medium=cpc&utm_campaign=14712539458&utm_content=546922691691&utm_term=dsa-42114402488_&GL_Ad_ID=546922691691&GL_Campaign_ID=14712539458&gclid=CjwKCAiAzrWOBhBjEiwAq85QZ5Ur6faaX4PoxOo8E-GLFt5126iJjPPYTPL0kxmObd1s78P6xre93hoCDzUQAvD_BwE";
+    const selector = "#leftColumn > div.largeTitle > article > div.textDiv";
+    const response = await axios.get(URL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const news = [];
+
+    const keys = ["heading", "author", "desc"];
+
+    $(selector).each((parentIndex, parentEl) => {
+      const articleObj = {};
+      let keyIndex = 0;
+      let link;
+      $(parentEl)
+        .children()
+        .each((childIndex, childEl) => {
+          const article = $(childEl).text();
+          link = $(childEl).attr("href");
+          articleObj[keys[keyIndex++]] = article;
+          if (link) articleObj.link = link;
+        });
+      news.push(articleObj);
+    });
+
+    res.send({ baseLink: URL, news });
   },
 };

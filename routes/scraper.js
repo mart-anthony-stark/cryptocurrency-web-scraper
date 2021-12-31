96c0e746055ac2e98dde46c36a23d9fbbda16894
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-31 14:14:24 +0800

    router

--- routes/scraper.js
+++ routes/scraper.js
@@ -1,6 +1,3 @@
 const router = require("express").Router();
 const { getTopCoins, getNews } = require("../controllers/scraper");
 
-router.get("/", getTopCoins);
-router.get("/news", getNews);
-module.exports = router;

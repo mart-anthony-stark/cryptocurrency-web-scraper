304c73f8f701a06a2caa536e07db31ee84700142
mart-anthony-stark <martsalazar0@gmail.com>
2021-12-31 14:14:34 +0800

    Add routes

--- routes/scraper.js
+++ routes/scraper.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
 const { getTopCoins, getNews } = require("../controllers/scraper");
 
+router.get("/", getTopCoins);
+router.get("/news", getNews);
+module.exports = router;
